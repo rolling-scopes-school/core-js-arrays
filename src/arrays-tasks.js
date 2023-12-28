77a3a6c7faede5db099de1cfb16ccb4174f8fa28
Lilo002 <lisabasarab@gmail.com>
2023-12-28 15:20:54 +0300

    feat: add removeFalsyValues's solution (Thu, Dec 28, 2023  3:20:53 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -100,8 +100,8 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((item) => item);
 }
 
 /**
