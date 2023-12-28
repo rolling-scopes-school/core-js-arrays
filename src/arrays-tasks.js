971a5de94e3a653317e0624f967c9ad6d786dfc6
Lilo002 <lisabasarab@gmail.com>
2023-12-28 14:50:03 +0300

    feat: add solution to generate

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -34,8 +34,11 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len);
+  arr.fill(1);
+
+  return arr.map((item, index) => index * 2 + 1);
 }
 
 /**
