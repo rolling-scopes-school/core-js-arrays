763b4c0dadee71a00aeb58c7aae168b5c67af025
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:22:07 +0300

    feat: add propagateItemsByPositionIndex's solution (Thu, Dec 28, 2023  6:22:07 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -272,8 +272,20 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const newArr = arr.reduce((acc, cur, index) => {
+    let counter = 0;
+    function addingItemByIndex(item, i, array) {
+      if (counter < i + 1) {
+        array.push(item);
+        counter += 1;
+        addingItemByIndex(item, i, array);
+      }
+    }
+    addingItemByIndex(cur, index, acc);
+    return acc;
+  }, []);
+  return newArr;
 }
 
 /**
