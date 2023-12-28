13375076b2f1fe3556f11dc844390b6a70c1a64d
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:33:22 +0300

    feat: add getPositivesCount's solution (Thu, Dec 28, 2023  6:33:22 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -321,8 +321,11 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.reduce(
+    (acc, cur) => (cur > 0 && typeof cur === 'number' ? acc + 1 : acc),
+    0
+  );
 }
 
 /**
