ce42b210c4aa514859ea276d934a012a5e2cc110
Lilo002 <lisabasarab@gmail.com>
2023-12-28 19:10:05 +0300

    feat: add getIntervalArray's solution (Thu, Dec 28, 2023  7:10:05 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -511,8 +511,10 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array(end - start + 1)
+    .fill(start)
+    .map((item, index) => item + index);
 }
 
 /**
