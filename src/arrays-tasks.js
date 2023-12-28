5daedf78b31b6130204393df6751511ed84a8437
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:40:01 +0300

    feat: add getFalsyValuesCount's solution (Thu, Dec 28, 2023  6:40:00 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -385,8 +385,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((item) => !item).length;
 }
 
 /**
