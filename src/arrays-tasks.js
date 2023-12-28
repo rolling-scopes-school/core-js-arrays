62c9ecb0998df9f554bb3b858ff8fe09a9f340ed
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:05:55 +0300

    feat: add getSecondItems's solution (Thu, Dec 28, 2023  6:05:55 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -254,8 +254,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((item, index) => index % 2 !== 0);
 }
 
 /**
