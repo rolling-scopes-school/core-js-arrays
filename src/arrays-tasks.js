e61585abf5e177f08dd357c7c80923f7c92895ca
Lilo002 <lisabasarab@gmail.com>
2023-12-28 15:00:47 +0300

    feat: add solution to doubleArray (Thu, Dec 28, 2023  3:00:47 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -53,8 +53,8 @@ function generateOdds(len) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return arr.concat(arr);
 }
 
 /**
