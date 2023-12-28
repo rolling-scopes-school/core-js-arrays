3cf227ea75a07bdd1f9044075bca1f5caa319b33
Lilo002 <lisabasarab@gmail.com>
2023-12-28 20:45:30 +0300

    fix: delete testing js file (Thu, Dec 28, 2023  8:45:29 PM)

--- teat.js
+++
@@ -1,9 +0,0 @@
-function swapHeadAndTail(arr) {
-    const halfOfArrayLength = Math.floor(arr.length / 2);
-    const leftPart = arr.slice(0, halfOfArrayLength);
-    console.log(leftPart)
-    const rightPart = arr.slice(halfOfArrayLength);
-    return rightPart.concat(leftPart);
-  }
-
-  console.log(swapHeadAndTail([1,2, 3]))
