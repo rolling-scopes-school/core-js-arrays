f5841088e2327a7773b263fcc07c89b2a094b3e8
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:41:51 +0300

    feat: add findAllOccurrences's solution (Thu, Dec 28, 2023  6:41:51 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -403,8 +403,8 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, i) {
+  return arr.filter((item) => item === i).length;
 }
 
 /**
