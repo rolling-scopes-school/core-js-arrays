e01839209eed7f7ad44bb9dc135eef4afa645fdf
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:37:16 +0300

    feat: add sortDigitNamesByNumericOrder's solution (Thu, Dec 28, 2023  6:37:16 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -341,8 +341,20 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const arrOfDigits = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => arrOfDigits.indexOf(a) - arrOfDigits.indexOf(b));
 }
 
 /**
