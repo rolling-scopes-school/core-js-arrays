9f5ff989e599b860a5fd5f8b1b9a03a9fca1c8f7
Lilo002 <lisabasarab@gmail.com>
2023-12-28 14:27:53 +0300

    feat: add solution to findElement task (Thu, Dec 28, 2023  2:27:53 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -19,8 +19,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.findIndex((item) => item === value);
 }
 
 /**
