a70e425e9b4a3686ba495c340ee54f8aa1ac915f
Lilo002 <lisabasarab@gmail.com>
2023-12-28 15:18:51 +0300

    feat: add getArrayOfStrings's solution (Thu, Dec 28, 2023  3:18:51 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -83,8 +83,8 @@ function getArrayOfPositives(arr) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  return arr.filter((item) => typeof item === 'string');
 }
 
 /**
