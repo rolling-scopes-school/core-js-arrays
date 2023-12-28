9bb3e16aa68c463148677d8726154b39130ab1e9
Lilo002 <lisabasarab@gmail.com>
2023-12-28 18:38:43 +0300

    feat: add getItemsSum's solution (Thu, Dec 28, 2023  6:38:43 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -369,8 +369,8 @@ function sortDigitNamesByNumericOrder(arr) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  return arr.reduce((acc, cur) => acc + cur, 0);
 }
 
 /**
