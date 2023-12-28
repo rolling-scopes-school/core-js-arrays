8befcec3edb359d54c5085313e4841da050511ec
Lilo002 <lisabasarab@gmail.com>
2023-12-28 20:09:58 +0300

    feat: add selectMany's solution (Thu, Dec 28, 2023  8:09:58 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -588,8 +588,8 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 /**
