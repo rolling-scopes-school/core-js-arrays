39aaa611d9daae5a290d24ab5f2911fb94767d47
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:22:24 +0300

    feat: removeFalsyValues implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -87,8 +87,8 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((el) => Boolean(el) === true);
 }
 
 /**
