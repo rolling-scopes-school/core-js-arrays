10b468c1e574d4d9b986efedb5901bb941a8092b
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:30:30 +0300

    feat: doubleArray implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -210,8 +210,8 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return arr.concat(arr);
 }
 
 /**
