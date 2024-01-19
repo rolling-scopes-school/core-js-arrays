c4a604fe231a426a54a714f2ad0abcbcea6ca7d0
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:28:53 +0300

    feat: getTail implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -194,8 +194,8 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(arr.length - n, arr.length);
 }
 
 /**
