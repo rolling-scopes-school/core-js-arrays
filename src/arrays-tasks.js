f1bbf0f57525009d36309affcb93f6837514e8fe
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:41:22 +0300

    feat: selectMany implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -290,8 +290,9 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  const a = arr.map(childrenSelector);
+  return a.flat(Infinity);
 }
 
 /**
