11fe5856912424480474a9ce0a9e4797eddc5726
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:27:51 +0300

    feat: getHead implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -179,8 +179,8 @@ function insertItem(arr, item, index) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 /**
