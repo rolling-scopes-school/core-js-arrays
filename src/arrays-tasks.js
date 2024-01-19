73aa696f25aab72315ab95f17df2648a8200ea75
Natalia Furmanova <nfurmanova@tula.co>
2024-01-20 01:06:03 +0300

    feat: isSameLength implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -119,8 +119,16 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = 0;
+  const num =
+    arr.length === 0
+      ? 0
+      : arr.reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          sum
+        ) / arr.length;
+  return Math.fround((num * 100) / 100);
 }
 
 /**
@@ -133,8 +141,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((el) => el.length === arr[0].length);
 }
 
 /**
