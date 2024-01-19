16bfec0662b7a3d45c92b5225adc8eee8563ca95
Natalia Furmanova <nfurmanova@tula.co>
2024-01-20 01:29:12 +0300

    feat: getFalsyValuesCount implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -380,8 +380,8 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((el) => Boolean(el) === false).length;
 }
 
 /**
