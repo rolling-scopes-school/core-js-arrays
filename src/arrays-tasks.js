1e7e56dd5c5b0cd547051cc602348206ffa7c37f
Natalia Furmanova <nfurmanova@tula.co>
2024-01-19 22:19:18 +0300

    feat: findElement implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -53,8 +53,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
