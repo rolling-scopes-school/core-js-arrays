6a3e63251580b7fe7e51ab097ae7cb21a2a2f937
Natalia Furmanova <nfurmanova@tula.co>
2024-01-23 16:59:53 +0300

    feat: getMaxItems implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -454,8 +454,11 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr
+    .sort((a, b) => a - b)
+    .reverse()
+    .slice(0, n);
 }
 
 /**
