1e05f12a3eb11b4c0e4b338a568ff121661f7cb2
Natalia Furmanova <nfurmanova@tula.co>
2024-01-23 17:14:56 +0300

    feat: propagateItemsByPositionIndex implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -506,8 +506,13 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr
+    .reduce((acc, el, index) => {
+      acc.push(new Array(index + 1).fill(el));
+      return acc;
+    }, [])
+    .flat();
 }
 
 /**
