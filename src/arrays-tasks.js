67c4792fad1aad54d5363f9efadffd14b52289a0
Natalia Furmanova <nfurmanova@tula.co>
2024-01-23 15:23:51 +0300

    feat: getIndicesOfOddNumbers implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -419,8 +419,11 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((arr, el, index) => {
+    if (el % 2 !== 0) arr.push(index);
+    return arr;
+  }, []);
 }
 
 /**
