35f55faabb40898dea25efece6e7068cdccac5be
Natalia Furmanova <nfurmanova@tula.co>
2024-01-24 16:20:41 +0300

    feat: calculateBalance implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -323,8 +323,13 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, el) => {
+    const balance = el.reduce((acc1, el1) => {
+      return acc1 - el1;
+    });
+    return acc + balance;
+  }, 0);
 }
 
 /**
