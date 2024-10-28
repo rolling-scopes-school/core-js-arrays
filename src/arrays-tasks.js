8387ee510e118e6544c506e2dd16960e9dc508b8
Natalia Furmanova <nfurmanova@tula.co>
2024-10-28 14:34:32 +0300

    feat: findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -502,8 +502,8 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr2.filter((x) => arr1.includes(x));
 }
 
 /**
